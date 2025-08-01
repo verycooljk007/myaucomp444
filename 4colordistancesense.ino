ec1882bc78f874a35f823ae224057febe9c2c012
Meshaal Khan <62823214+verycooljk007@users.noreply.github.com>
2025-07-31 21:55:04 -0400

    Add files via upload

---
+++ 4colordistancesense.ino
@@ -0,0 +1,90 @@
+/*
+  SparkFun Inventor’s Kit
+  Circuit 3B-Distance Sensor
+
+  Control the color of an RGB LED using an ultrasonic distance sensor.
+
+  This sketch was written by SparkFun Electronics, with lots of help from the Arduino community.
+  This code is completely free for any use.
+
+  View circuit diagram and instructions at: https://learn.sparkfun.com/tutorials/sparkfun-inventors-kit-experiment-guide---v41
+  Download drawings and code at: https://github.com/sparkfun/SIK-Guide-Code
+*/
+
+const int trigPin = 11;           //connects to the trigger pin on the distance sensor
+const int echoPin = 12;           //connects to the echo pin on the distance sensor
+
+const int redPin = 3;             //pin to control the red LED inside the RGB LED
+const int greenPin = 5;           //pin to control the green LED inside the RGB LED
+const int bluePin = 6;            //pin to control the blue LED inside the RGB LED
+
+float distance = 0;               //stores the distance measured by the distance sensor
+
+void setup()
+{
+  Serial.begin (9600);        //set up a serial connection with the computer
+
+  pinMode(trigPin, OUTPUT);   //the trigger pin will output pulses of electricity
+  pinMode(echoPin, INPUT);    //the echo pin will measure the duration of pulses coming back from the distance sensor
+
+  //set the RGB LED pins to output
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop() {
+  distance = getDistance();   //variable to store the distance measured by the sensor
+
+  Serial.print(distance);     //print the distance that was measured
+  Serial.println(" in");      //print units after the distance
+
+  if (distance <= 10) {
+    // RED (close)
+    analogWrite(redPin, 255);
+    analogWrite(greenPin, 0);
+    analogWrite(bluePin, 0);
+
+  } else if (distance > 10 && distance <= 20) {
+    // YELLOW (medium)
+    analogWrite(redPin, 255);
+    analogWrite(greenPin, 50);
+    analogWrite(bluePin, 0);
+
+  } else if (distance > 20 && distance <= 30) {
+    // PURPLE (farther)
+    analogWrite(redPin, 255);
+    analogWrite(greenPin, 0);
+    analogWrite(bluePin, 255);
+
+  } else {
+    // GREEN (farthest)
+    analogWrite(redPin, 0);
+    analogWrite(greenPin, 255);
+    analogWrite(bluePin, 0);
+  }
+
+  delay(50);      //delay 50ms between each reading
+}
+
+//------------------FUNCTIONS-------------------------------
+
+//RETURNS THE DISTANCE MEASURED BY THE HC-SR04 DISTANCE SENSOR
+float getDistance()
+{
+  float echoTime;                   //variable to store the time it takes for a ping to bounce off an object
+  float calculatedDistance;         //variable to store the distance calculated from the echo time
+
+  //send out an ultrasonic pulse that's 10ms long
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  echoTime = pulseIn(echoPin, HIGH);      //use the pulsein command to see how long it takes for the
+                                          //pulse to bounce back to the sensor
+
+  calculatedDistance = echoTime / 148.0;  //calculate the distance of the object that reflected the pulse (half the bounce time multiplied by the speed of sound)
+
+  return calculatedDistance;              //send back the distance that was calculated
+}
+
